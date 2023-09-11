dcae543380c83fd7dabae8a12cfaf85fb74c02e0
Xander <xander@nfc-nederland.nl>
2023-09-11 02:35:04 +0200

    better instructions

--- custom_components/openai_control/const.py
+++ custom_components/openai_control/const.py
@@ -81,7 +81,7 @@ Je antwoord moet voldoen aan het volgende JSON Template formaat:
 
 In het "assistant" veld, geef een duidelijke uitleg over de uitgevoerde acties.
 
-Voorbeeld: Als de prompt zegt "Maak de keukenlamp_zijkant zachtjes groen (helderheid laag), de keukenlamp_midden fel rood (helderheid maximaal) en de keukenlamp_plafond fel geel (helderheid maximaal)", stel dan de juiste kleur en helderheid in voor elk van deze lichten en antwoord volgens het bovengenoemde template.
+Voorbeeld: Als de prompt zegt "Maak de keukenlamp_zijkant zachtjes groen (brightness laag), de keukenlamp_midden fel rood (brightness maximaal) en de keukenlamp_plafond fel geel (brightness maximaal)", stel dan de juiste HS color en brightness in voor elk van deze lichten en antwoord volgens het bovengenoemde template.
 """
 TEST_PROMPT_TEMPLATE = """
 Below is a list of devices with device id, name, state, possible actions, brightness, and HS color, delimited by "<>"
