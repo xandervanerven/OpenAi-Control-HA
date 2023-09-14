2e752c90d4ceb1c7dd0b6102d85a5894a74a73f2
Xander <xander@nfc-nederland.nl>
2023-09-15 01:12:31 +0200

    fix

--- custom_components/openai_control/config_flow.py
+++ custom_components/openai_control/config_flow.py
@@ -147,6 +147,7 @@ def openai_config_option_schema(options: MappingProxyType[str, Any]) -> dict:
         ): str,
         vol.Optional(
             LANGUAGE_AND_MODE,
+            default=DEFAULT_LANGUAGE_AND_MODE,
         ): vol.In(LANGUAGE_AND_MODE_OPTIONS),
         vol.Optional(
             CONF_MAX_TOKENS,
